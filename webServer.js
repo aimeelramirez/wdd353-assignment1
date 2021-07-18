ccd9be394ed25258609ce0a08e45a5b2d9c2f2e7
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-07-18 17:32:07 -0400

    some data

--- webServer.js
+++ webServer.js
@@ -2,18 +2,23 @@ const https = require('https');
 const fs = require('fs')
 const options = require('./config')
 const path = require('path');
-// const url = require('url')
-
-
 //set port
 const port = 8080
+let sendUsers = `let users = [
+    { "0": "ales" },
+    { "1": "mila" },
+    { "2": "gilbert" },
+    { "3": "gilbert jr." },
+    { "4": "kristine" },
+    { "5": "chris" },
+    { "6": "aimee" }
+]`
 
+console.log(`Starting up server.... https://localhost:${port}`)
 //ssl
 https
     .createServer(options, (req, res) => {
-
         console.log(`Your server is running on port ${port}`);
-        //let parsed = url.parse(req.url) // The declaration was marked as deprecated here.//
         let filenameUrl = path.join(req.url)
         let filename = path.parse(filenameUrl)
 
@@ -34,7 +39,6 @@ https
         if (f) {
             fs.readFile(f, ((err, data) => {
                 if (page) {
-                    // console.log(mimeTypes.hasOwnProperty(ext))
                     if (mimeTypes.hasOwnProperty(ext)) {
                         res.writeHead(200, {
                             'Content-Type': ext
@@ -43,18 +47,32 @@ https
                             //get the script to read correctly
                             var writeScript = fs.readFileSync(f);
                             res.write(writeScript);
+                            res.write(sendUsers)
+                            res.write('\nfor(let i = 0; i < users.length; i++){'
+                                + '\n for(const [key, value] of Object.entries(users[i])){' +
+                                '\ndocument.querySelector("#list-users").innerHTML +="<li>"+ `${key}: ${value}` +"</li>";' +
+                                ' \n//console.log(`${key}: ${value}`); \n } '
+                                + '}')
                         } else if (f !== 'public/js/scripts.js') {
-                            //i want the styles to load 
-                            var writeStyles = fs.readFileSync('public/css/styles.css')
-                            res.write('<style>' + writeStyles + '</style>')
                             //i want the files to be written if not on page
                             var writeF = fs.readFileSync(f, 'utf8')
-                            res.write(writeF);
+                            if (f === 'public/css/styles.css') {
+                                //i want the styles to load 
+                                var writeStyles = fs.readFileSync('public/css/styles.css')
+                                res.write(writeStyles)
+
+                            } else {
+                                var writeStyles = fs.readFileSync('public/css/styles.css')
+                                // if (writeStyles !== null) {
+                                // }
+                                res.write('<style>' + writeStyles + '</style>');
+                                res.write(writeF)
+                            }
+
+
                             //write localhost
                             res.write('<base href="https://localhost:8080/">')
                         }
-                        //verified data
-                        // console.log(data)
                         res.end();
                     }
                 }
