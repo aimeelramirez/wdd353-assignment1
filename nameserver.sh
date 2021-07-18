910d114452ec27a1c0ed9fe1e9beaf8fef908cc3
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-07-18 18:24:51 -0400

    readme youtube

---
+++ nameserver.sh
@@ -0,0 +1,10 @@
+ #!/bin/sh
+ FILE=/etc/resolv.conf
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else 
+   echo "$FILE does not exist."
+   sudo bash -c 'echo "nameserver 127.0.0.1" >> /etc/resolv.conf'
+   sudo bash -c 'echo "nameserver 8.8.8.8" >> /etc/resolv.conf'
+   sudo bash -c 'echo "nameserver 8.8.8.4" >> /etc/resolv.conf'
+fi
