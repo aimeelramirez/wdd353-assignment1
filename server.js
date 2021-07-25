a70fb21dd8702021d0ce4cb9e416bfbca04dda80
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-07-25 17:28:42 -0400

    node modules

--- server.js
+++ server.js
@@ -4,47 +4,66 @@
 
 const https = require('https');
 const fs = require('fs')
-const options = require('./config')
-const express = require("express");
+
+
 let ejs = require('ejs');
 const path = require('path');
-const url = require('url');
-const router = new express.Router();
+
+// const sessions = require('express-session')
+// const url = require('url');
+//cert
+const options = require('./config')
+//call api to get to server from dir
 const api = require('./public/js/api')
+//controller on signup for logic
 const controller = require("./public/js/auth/controller")
+
+
 // Nodejs encryption with CTR
 const crypto = require('crypto');
 const algorithm = 'aes-256-cbc';
 const key = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
-//get api
 
-const app = express();
+
+//get api express
+const express = require("express"),
+    app = express();
+const router = new express.Router();
 
 //set port
 const port = 8080
 app.set("port", port);
 
 
+//check dirname
+// console.log(process.cwd())
 
-console.log(process.cwd())
-
-// // get the view pages of htmls
-const viewsPath = path.join(__dirname, './views');
-
-const publicPath = path.join(__dirname, './public');
-// const partialsPath = path.join(__dirname, './partials');
-
+// get the view pages
+const viewsPath = path.join(__dirname, 'views');
+const publicPath = path.join(__dirname, 'public');
 //set views
 app.set('views', viewsPath);
-// app.set('partials', partialsPath);
-app.set('ejs', ejs.renderFile);
-
-
-app.set('view engine', 'ejs');
-app.engine('ejs', require('ejs').__express);
+//body parse update
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+// engines
+// app.engine('html', es6Renderer);
+app.engine('ejs', require('ejs').__express);
+app.engine('html', ejs.renderFile);
+//view engine
+app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+// static
+// get public path
+app.use(express.static(publicPath));
+// //gets the styles to be dynamic
+app.use(express.static(path.join(__dirname, 'public/css')));
+// //gets  the js to be dynamic
+app.use(express.static(path.join(__dirname, 'public/js')));
+//use router
+app.use('/', router)
+
 /*  Encrypt  */
 
 function encrypt(text) {
@@ -63,13 +82,13 @@ function decrypt(text) {
     return decrypted.toString();
 }
 
-var hw = encrypt("Some serious stuff")
-console.log(hw)
-console.log(decrypt(hw))
+// var hw = encrypt("Some serious stuff")
+// console.log(hw)
+// console.log(decrypt(hw))
 
 router.get("/dashboard", function (req, res) {
     // console.log(req.body)
-    res.render('dashboard.html', {
+    res.render('dashboard', {
         title: 'DASHBOARD',
         message: "take a look at the console."
     })
@@ -78,7 +97,7 @@ router.get("/dashboard", function (req, res) {
 })
 router.get("/data", function (req, res) {
     console.log(JSON.stringify(req.body))
-    res.render('404.html', {
+    res.render('404', {
         title: 'DATA',
         message: "take a look at the console."
     })
@@ -89,7 +108,7 @@ router.get("/data", function (req, res) {
 router.post("/data", function (req, res) {
     console.log(req.body)
 
-    res.render('404.html', {
+    res.render('404', {
         title: 'DATA',
         message: JSON.stringify(req.body)
     })
@@ -104,79 +123,66 @@ router.post("/data", function (req, res) {
         }
     });
 })
-// router.get("/auth", (req, res) => {
-//     // // res.header("Access-Control-Allow-Origin", "*") // update to match the domain you will make the request from
-//     // // res.header(
-//     // //     "Access-Control-Allow-Origin",
-//     // //     "Access-Control-Allow-Headers",
-//     // //     "x-access-token, Origin, X-Requested-With, Content-Type, Accept",
-//     // // )
 
-//     // // res.setHeader("Content-Type", "text/html");
-//     // console.log(JSON.stringify(req.body))
-//     // res.render('dashboard.html', {
-//     //     title: 'DASHBOARD',
-//     //     message: "take a look at the console."
-//     // })
-
-// })
 
 router.post("/auth", (req, res) => {
+    //get auth
+    controller.signup(req, res)
+    res.end();
 
+})
+
+router.post("/login", (req, res) => {
+    //get auth
     controller.signup(req, res)
     res.end();
 
 })
 router.get("/profile", (req, res) => {
     console.log('Sub Pages');
-    res.render('profile.html', {
+    res.render('profile', {
         title: 'PROFILE',
         message: 'Page not found.'
     })
 })
 router.get("/overview", (req, res) => {
     console.log('Sub Pages');
-    res.render('overview.html', {
+    res.render('overview', {
         title: 'OVERVIEW',
         message: 'Page not found.'
     })
 })
 
-router.post("/404", (req, res) => {
-    res.render('404.html', {
-        title: '404',
-        message: 'Page not found.'
-    })
-})
-// router.get("/index", (req, res) => {
-//     res.render('index', {
-//         title: 'HOME',
-//         message: "Welcome!"
-//     })
-// })
-// router.get("/", (req, res) => {
-//     res.render('index', {
-//         title: 'HOME',
-//         message: "Welcome!"
-//     })
-// })
-// //get public path
-app.use(express.static(publicPath));
-// //gets the styles to be dynamic
-app.use(express.static(path.join(__dirname, 'public/css')));
-// //gets  the js to be dynamic
-app.use(express.static(path.join(__dirname, 'public/js')));
 
-app.use('/', router)
-app.get('/', function (req, res) {
-    res.render(viewsPath + '/*');
+
+router.get("/index", (req, res) => {
+    res.render('index', {
+        title: 'HOME',
+        message: "Welcome!"
+    })
+})
+router.get('/', function (req, res) {
+    res.render('index', {
+        title: 'HOME',
+        message: 'Welcome!'
+    });
 });
-app.get('/*', (req, res) => {
-    res.render('404.html', {
+
+router.get("/404", (req, res) => {
+    res.render('404', {
         title: '404',
         message: 'Page not found.'
     })
 })
+
+router.get('/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        message: 'Page not found.'
+    })
+})
+
+
 //ssl
 https
     .createServer(options, app, api, (req, res) => {
